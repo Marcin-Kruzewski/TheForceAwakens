580b911a60535f3e06599dcbc6dc1970a7f674ca
MarcinKruzewski <kruzewski@klukowo.com>
2016-05-21 15:28:54 +0200

    Initial commit with task content

---
+++ pl/edu/pjwstk/poj/theforceawakens/Characters/Race.java
@@ -0,0 +1,8 @@
+package pl.edu.pjwstk.poj.theforceawakens.Characters;
+
+import java.util.List;
+
+public interface Race {
+	
+	public List<String> GetAbilities();
+}
---
+++ pl/edu/pjwstk/poj/theforceawakens/PlayerCharacter.java
@@ -0,0 +1,5 @@
+package pl.edu.pjwstk.poj.theforceawakens;
+
+public class PlayerCharacter {
+	
+}
